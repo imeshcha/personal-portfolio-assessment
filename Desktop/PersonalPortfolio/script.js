0ce4182abebf0d066e87b72117af15938622361f
Imesh Chathura <saimeshchathurak@gmail.com>
2026-01-11 01:42:43 +0530

    update design

--- Desktop/PersonalPortfolio/script.js
+++ Desktop/PersonalPortfolio/script.js
@@ -28,3 +28,38 @@ textDisplay.textContent = phrases[0];
 
 // Change text every 3 seconds
 setInterval(updateText, 3000);
+
+// Contact Form Handling
+var form = document.getElementById("contact-form");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Feedback submitted!";
+            status.style.color = "#4bb543"; // Success green
+            form.reset();
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
+                } else {
+                    status.innerHTML = "Oops! There was a problem submitting your form";
+                }
+                status.style.color = "#ff0000"; // Error red
+            })
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form";
+        status.style.color = "#ff0000"; // Error red
+    });
+}
+form.addEventListener("submit", handleSubmit);
